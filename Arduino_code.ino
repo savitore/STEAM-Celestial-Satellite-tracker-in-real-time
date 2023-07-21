a71d44cc1b0167d537a5c8da7a99a313b8889827
Krishna Agrawal <21je0482@iitism.ac.in>
2023-07-21 21:58:26 +0530

    Update Arduino_code.ino

--- Arduino_code.ino
+++ Arduino_code.ino
@@ -1,6 +1,7 @@
 #include <ArduinoP13.h>
 #include <Servo.h>
 #include <TimeLib.h>
+#include <Math.h>
 
 Servo elevation; 
 Servo azimuth;
@@ -10,12 +11,13 @@ String l2;
 String str;
 String str1;
 String read;
-int CurrentHour=16; 
-int CurrentMin=51; 
+int CurrentHour=14; 
+int CurrentMin=57; 
 int CurrentSec=5; 
-int CurrentDay=8; 
+int CurrentDay=9; 
 int CurrentMonth=7; 
 int CurrentYear=2023;
+const float R = 6371.0;
 char acBuffer[20];
 const char  *pcMyName = "krishna";     // Observer name
 double dMyLAT;    
@@ -37,6 +39,40 @@ void setup() {
   azimuth.write(apos);
   Serial.begin(9600); //default baud rate for bt 38400
   delay(5000); 
+}
+
+// Function to calculate the azimuth, elevation, and range angles
+void calculateAngles(double lat_ref, double lon_ref, double lat_sat, double lon_sat,
+                     double& azimuth, double& elevation) {
+  // Convert degrees to radians
+  lat_ref = radians(lat_ref);
+  lon_ref = radians(lon_ref);
+  lat_sat = radians(lat_sat);
+  lon_sat = radians(lon_sat);
+
+
+  // Calculate differences in longitude, latitude, and altitude
+  double d_lon = lon_sat - lon_ref;
+  double d_lat = lat_sat - lat_ref;
+
+
+  // Calculate azimuth angle in radians
+  float x = cos(lat_ref) * sin(lat_sat) - sin(lat_ref) * cos(lat_sat) * cos(d_lon);
+  float y = cos(lat_sat) * sin(d_lon);
+  float azimuth_rad = atan2(y, x);
+
+  // Convert azimuth angle from radians to degrees
+  azimuth = degrees(azimuth_rad);
+  if (azimuth < 0) {
+    azimuth += 360.0;
+  }
+
+  // Calculate elevation angle in radians
+  float z = sin(lat_ref) * sin(lat_sat) + cos(lat_ref) * cos(lat_sat) * cos(d_lon);
+  float elevation_rad = asin(z);
+
+  // Convert elevation angle from radians to degrees
+  elevation = degrees(elevation_rad);
 }
 
 void loop() {
@@ -112,6 +148,33 @@ void loop() {
       part1 = strtok(NULL, delimiter1);
     }
      setTime(CurrentHour,CurrentMin,CurrentSec,CurrentDay,CurrentMonth,CurrentYear);//set the current time which we get from flutter app
+     Serial.println("");
+     Serial.println("TLE:-");  
+     Serial.println(l0);
+     Serial.println(l1);
+     Serial.println(l2);
+     Serial.println("Current date and time:- ");
+     Serial.print(CurrentHour);
+     Serial.print(":");
+     Serial.print(CurrentMin);
+     Serial.print(":");
+     Serial.print(CurrentSec);
+     Serial.print(" , ");
+     Serial.print(CurrentDay);
+     Serial.print("-");
+     Serial.print(CurrentMonth);
+     Serial.print("-");
+     Serial.print(CurrentYear);
+     Serial.println("");
+     Serial.println("Latitude: ");
+     Serial.print(dMyLAT);
+     Serial.println("");
+     Serial.println("Longitude: ");
+     Serial.print(dMyLON);
+     Serial.println("");
+     Serial.println("Altitude: ");
+     Serial.print(dMyALT);
+     Serial.println("");
      read="done";
   }
      if(read=="done"){
@@ -119,7 +182,7 @@ void loop() {
      char *tleName = l0.c_str();
      char *tlel1 = l1.c_str();
      char *tlel2 = l2.c_str();
-     
+
      //using time library to calcuate azimuth and elevation for ongoing time
      int iYear    = year(); // Set start year  
      int iMonth   = month(); // Set start month  
@@ -134,36 +197,40 @@ void loop() {
       MyTime.ascii(acBuffer);
       MySAT.predict(MyTime);              // Predict ISS for specific time  
       MySAT.latlon(dSatLAT, dSatLON);     // Get the rectangular coordinates  
-      MySAT.elaz(MyQTH, dSatEL, dSatAZ);  // Get azimut and elevation for MyQTH
+  
       delay(500);
+      
+      Serial.println(dSatLAT);
+      Serial.println(dSatLON);
+
+      calculateAngles(dMyLAT, dMyLON, dSatLAT, dSatLON, dSatAZ, dSatEL);
+
       // Servo calculation  
       epos = (int)dSatEL; 
-      apos = (int)dSatAZ;
+      apos = (int)dSatAZ;    
+
       if (epos < 0) {
-        // Serial.write("Out of range, Can't view satellite.");
-      } else {
-        // Serial.write("In range, Satellite can be viewed.");  
+        Serial.println("Out of range, Can't view satellite.");
+      } 
+      else {
+        Serial.println("In range, Satellite can be viewed.");
         if(apos < 180) {  
           apos = abs(180 - (apos)); 
           epos = 180-epos; 
-        } else {  
+        } 
+        else {  
           apos = (360-apos); 
           epos = epos; 
         }  
-      azimuth.write(apos);  
-      delay(15); 
-      elevation.write(epos); 
-      }
-    Serial.println(l0);
-    Serial.println(l1);
-    Serial.println(l2);
-    Serial.println(dMyLAT);
-    Serial.println(dMyLON);
-    Serial.println(dMyALT);
-    Serial.println(dSatEL);
-    Serial.println(dSatAZ);
-    Serial.println(apos);
-    Serial.println(epos);
+        azimuth.write(apos);  
+        delay(15); 
+        elevation.write(epos); 
+      }        
+
+      Serial.print("azimuth: ");
+      Serial.println(apos);
+      Serial.print("elevation: ");
+      Serial.println(epos);
      }
     
 }
