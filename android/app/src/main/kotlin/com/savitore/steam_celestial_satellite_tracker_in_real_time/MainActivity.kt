60f8b0fa7100f2f638c588a52d79ede3c7fa4c40
savitore <21je0482@iitism.ac.in>
2023-08-09 15:16:36 +0530

    changed package name

--- android/app/src/main/kotlin/com/savitore/steam_celestial_satellite_tracker_in_real_time/MainActivity.kt
+++ android/app/src/main/kotlin/com/savitore/steam_celestial_satellite_tracker_in_real_time/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.steam_celestial_satellite_tracker_in_real_time
+package com.savitore.steam_celestial_satellite_tracker_in_real_time
 
 import io.flutter.embedding.android.FlutterActivity
 
